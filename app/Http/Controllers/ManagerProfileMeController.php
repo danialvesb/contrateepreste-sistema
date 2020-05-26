adb8e4e855549668d2a80a85e2a617d35ed50791
danialvesb <dan78615>
2020-05-25 21:42:38 -0300

    Finalizar lógica de upload de fotos

---
+++ app/Http/Controllers/ManagerProfileMeController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ManagerProfileMeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return void
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+        $data = $request->all();
+        $user = auth()->user();
+        $newMobile = $data['mobile'];
+        $newCity = $data['city'];
+        $newUf = $data['uf'];
+        $newDistrict = $data['district'];
+
+        if($request->hasFile('photo') && $request->file('photo')->isValid()){
+
+            if ($user->photo) {
+                $name = $user->photo;
+
+                $request->photo->storeAs('/images/profile', $data['photo']);
+            }else {
+                $name = $user->id.Str::kebab($user->name);
+                $name = preg_replace('/[^a-zA-Z0-9_]/', '', $name);
+                $extenstion = $request->photo->extension();
+                $name = $name.".".$extenstion;
+
+                $name = $request->photo->storeAs('/images/profile', $name);
+
+            }
+            print_r($name);
+
+
+
+            if ($name != $user->photo) {
+                DB::table('users')
+                    ->where('id', $user->id)
+                    ->update(['photo' => $name]);
+            }
+
+            if ($name) {
+                return response()->json('sucess');
+            }else {
+                return response()->json('error');
+            }
+        }
+        return response()->json('sucess');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return void
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
